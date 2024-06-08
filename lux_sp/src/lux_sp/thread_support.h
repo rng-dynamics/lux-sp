bf4b54d22f479c10ab51510ba3e8d1b4570689c6
Alexander Scharinger <rng.dynamics@gmail.com>
2024-06-08 22:19:29 +0200

    remove outdated CreatAndStartThread

--- lux_sp/src/lux_sp/thread_support.h
+++ lux_sp/src/lux_sp/thread_support.h
@@ -25,35 +25,6 @@ public:
                                            &cpu_set) == 0;
   }
 
-  template <typename Fn, typename... Args>
-  [[nodiscard]] std::unique_ptr<std::thread>
-  CreateAndStartThread(int core_id, Fn &&fn, Args &&...args) noexcept {
-    auto is_running = std::atomic_bool{false};
-    auto is_failed = std::atomic_bool{false};
-
-    auto thread_body = [&] {
-      if (core_id >= 0 && !SetThreadAffinityToCore(core_id)) {
-        is_failed.store(true, std::memory_order_release);
-        return;
-      }
-      is_running.store(true, std::memory_order_release);
-      std::forward<Fn>(fn)((std::forward<Args>(args))...);
-    };
-
-    auto thread = std::make_unique<std::thread>(thread_body);
-
-    while (!is_running.load(std::memory_order_acquire) &&
-           !is_failed.load(std::memory_order_acquire)) {
-      using namespace std::literals::chrono_literals;
-      std::this_thread::sleep_for(100ms);
-    }
-
-    if (is_failed) {
-      thread->join();
-    }
-    return thread;
-  }
-
 private:
   std::unique_ptr<System> system_{};
 };
